816a117a33e7f984c3ec436914ad0f3fab44f5c8
Mucyo <arnaudmucyo3@gmail.com>
2023-03-18 05:14:35 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return (0);
+}
+
