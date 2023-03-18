75a9e6d2d5b05285ce3f18a0be1ede9042d91237
Mucyo <arnaudmucyo3@gmail.com>
2023-03-18 05:04:41 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
