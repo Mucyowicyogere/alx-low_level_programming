881a611d6b85f109feae991a2f3999f6ce8830bb
Mucyo <arnaudmucyo3@gmail.com>
2023-03-18 09:34:13 -0700

    print char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int n, digit;
+
+	for (int i = 1; i<27; i++)
+		putchar(i);
+	
+	return (0);
+}
+
