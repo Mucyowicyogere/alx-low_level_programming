f9492199bc72b034408a012980be5fc78e6d5bb9
Mucyo <arnaudmucyo3@gmail.com>
2023-03-18 05:27:51 -0700

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	
+	return (0);
+}
+
