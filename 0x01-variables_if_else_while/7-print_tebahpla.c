cc19b35e0793afc0eab9b9e5ef97aa7ca93ff2d4
Mucyo <arnaudmucyo3@gmail.com>
2023-03-20 15:05:59 -0700

    base 16

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
