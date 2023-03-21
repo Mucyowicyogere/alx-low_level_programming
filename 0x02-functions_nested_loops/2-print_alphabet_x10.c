95a899fa8ed512706a5d1c84df1aed0a49baddec
Mucyo <arnaudmucyo3@gmail.com>
2023-03-21 13:58:43 -0700

    alphabet redundancy

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*print_alphabet - to print alphabets
+*
+*/
+void print_alphabet(char a)
+{
+	int i = 0;
+
+	do {
+		putchar(a);
+		a++;
+		i++;
+	} while (i <= 9);
+	putchar('\n');
+}
+
