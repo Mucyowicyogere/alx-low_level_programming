38e0d837bd5773d7d76eac127c3b1e939d3dfa66
Mucyo <arnaudmucyo3@gmail.com>
2023-03-21 13:27:01 -0700

    alphabet printing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,28 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include "main.h"
 /**
  * main - entry point
  *
  *Return: always 0 (success)
  */
-void print_alphabet(void)
-{
-	char a = 'a';
-        int i = 0;
 
-	do {
-		putchar(a);
-                a++;
-                i++;
-        } while (i <= 25);
-       
-	return;
-}
 int main(void)
 {
 	print_alphabet();
 	putchar('\n');
 	return (0);
+}
+void print_alphabet(void)
+{
+        char a = 'a';
+        int i = 0;
+
+        do {
+                putchar(a);
+                a++;
+                i++;
+        } while (i <= 25);
+
+        return;
 }
 
