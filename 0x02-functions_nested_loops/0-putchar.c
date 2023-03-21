c1e43e85c132b71bb28f33da7a6fa5eb7cbf05f7
Mucyo <arnaudmucyo3@gmail.com>
2023-03-21 07:00:59 -0700

    putchar printing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	char[8] put = "_putchar";
+	int i = 0;
+
+	do {
+		putchar(put[i]);
+		i++;
+	} while (i <= 7);
+	putchar('\n');
+	return (0);
+}
+
