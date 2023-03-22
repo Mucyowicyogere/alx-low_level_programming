4457326c891bbca160e31e737517c1278b226da1
Mucyo <arnaudmucyo3@gmail.com>
2023-03-21 17:22:11 -0700

    lowercase check

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		_putchar(43);
 		return (1);
 	} else if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		return(-1);
 	}
-	putchar(48);
+	_putchar(48);
 	return(0);
 }
 
