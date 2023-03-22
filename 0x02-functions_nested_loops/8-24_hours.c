7f807fdcbb984add4655bf1725d2c79aec57477a
Mucyo <arnaudmucyo3@gmail.com>
2023-03-21 18:02:50 -0700

    minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*jack_bauer - to print minutes
+*@n : a number to be checked
+* Return:  0 
+*
+*/
+void jack_bauer(void)
+{
+	int i = 0, j = 0;
+
+	while (i<24)
+	{
+		while(j<=59)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar(i);
+			_putchar(':');
+			if ( j<10)
+				_putchar('0');
+			_putchar(j);
+		}
+	}
+}
