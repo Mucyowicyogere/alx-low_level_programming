beca41b32a3595c504becae0423d51031c9ee815
Mucyo <arnaudmucyo3@gmail.com>
2023-03-23 15:47:03 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,16 @@
  * @c : An int argument for the character to check
  * Return : 1 for uppercase and 0 otherwise
  */
-int _isupper(int c)
-{
-	if (c > 64 && c < 91)
-		return (1);
-	return 0;
+int void _isupper(int c)
+
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+
 }
 
