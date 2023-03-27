d3ad469730ac5c8d2f7a5a81c914a84a1211458e
Mucyo <arnaudmucyo3@gmail.com>
2023-03-27 15:10:31 -0700

    pointer 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include <string.h>
+/**
+ * _strlen - it computes the string length
+ * @str: String to compute its length
+ * Return: the string length
+ */
+int _strlen(char *str)
+{
+	return (strlen(str));
+}
