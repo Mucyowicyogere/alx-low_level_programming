21361fc0f56d4c2660cb137ab37b853bf26bd2e2
Mucyo <arnaudmucyo3@gmail.com>
2023-03-27 15:46:52 -0700

    pointer 5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - A function to print a character and skip the next
+ * @str: The string to print from
+ * Return: Nothing
+ */
+void puts2(char *s)
+{
+	int i, length;
+
+	length = strlen(s);
+	for (i = 0; i < length; i = i + 2)
+		printf("%c", s[i]);
+	printf("\n");
+}
