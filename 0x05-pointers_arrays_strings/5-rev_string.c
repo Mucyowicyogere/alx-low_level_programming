8d23946f14d02416ae17f5e646e273c45281438e
Mucyo <arnaudmucyo3@gmail.com>
2023-03-27 15:32:21 -0700

    pointer 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <string.h>
+/**
+ * rev_string - it reverses a string
+ * @str: String to be reversed
+ * Return: void
+ */
+void rev_string(char *str)
+{
+	int l = strlen(s);
+	int i, a;
+	char tmp;
+
+	a = l - 1;
+	for (i = 0; i < l / 2; i++)
+	{
+		tmp = str[i];
+		str[i] = str[a];
+		str[a--] = tmp;
+	}
+}
