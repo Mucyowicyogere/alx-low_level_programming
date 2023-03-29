b4b44f6544e075cebd8e8a582ceb5cd54847a84e
Mucyo <arnaudmucyo3@gmail.com>
2023-03-29 15:59:00 -0700

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcat - A function that concantenate 2 different strings
+ * @dest: First string
+ * @src: Second string
+ * Return: concantenation of strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
