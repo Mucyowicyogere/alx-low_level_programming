79c93c1147ac1fc43441a9a36362a5d8a6c34be1
Mucyo <arnaudmucyo3@gmail.com>
2023-03-29 16:17:14 -0700

    concat part

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcmp - A function to compare two strings
+ * @s1: The first string
+ * @s2: The second string
+ * Return: 0 only when strings matches, a -ve value when they do not match
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
