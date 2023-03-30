ef2a48accd1496f59a8f6d45ea24a662c0ce1009
Mucyo <arnaudmucyo3@gmail.com>
2023-03-29 18:04:09 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * string_toupper - a function that changes all lowercase letters of a string to uppercase * @src: a string to copy
+ * Return: Return char
+ */
+char *string_toupper(char *c)
+{
+	while (c != '\0')
+		return (c-32);
+	return;
+}
