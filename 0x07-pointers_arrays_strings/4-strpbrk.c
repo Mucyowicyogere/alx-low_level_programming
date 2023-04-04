b7a1f713ff1ede519e8469afea98feb44c82c284
Mucyo <arnaudmucyo3@gmail.com>
2023-04-03 17:42:37 -0700

    meme

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,9 @@
 #include <string.h>
 /**
  *_strpbrk - A function to search for the first occurence of a string in a string
- * @s: The string in which to search
- * @accept: The string to be searched
- * Return: A pointer to the byte in @s or NULL if the byter is not there
+ *@s: The string in which to search
+ *@accept: The string to be searched
+ *Return: A pointer to the byte in @s or NULL if the byter is not there
  */
 char *_strpbrk(char *s, char *accept)
 {
