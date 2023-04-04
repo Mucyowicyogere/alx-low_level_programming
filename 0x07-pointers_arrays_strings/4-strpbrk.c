afcf47e076573acbecbc61b615baebda4a1bcf17
Mucyo <arnaudmucyo3@gmail.com>
2023-04-03 18:41:11 -0700

    crack

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,10 @@
-#include "string.h"
-
+#include <string.h>
 /**
  * _strpbrk - A function to search for the first occurence of a string in a string
  * @s: The string in which to search
  * @accept: The string to be searched
  * Return: A pointer to the byte in @s or NULL if the byter is not there
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	return (strpbrk(s, accept));
