b5dd7b0af4ae9dd9c6d612d1c31e01d58054260b
Mucyo <arnaudmucyo3@gmail.com>
2023-04-03 18:43:23 -0700

    crack

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,31 @@
-#include <string.h>
+#include "main.h"
+
 /**
- * _strpbrk - A function to search for the first occurence of a string in a string
- * @s: The string in which to search
- * @accept: The string to be searched
- * Return: A pointer to the byte in @s or NULL if the byter is not there
- */
+ * _strpbrk - matches any character specified
+ * @s: This is the C string to be scanned.
+ * @accept:character in str1 that matches one of the characters in str2
+ *Return: string s that matches any character specified in accept
+ **/
+
 char *_strpbrk(char *s, char *accept)
 {
-	return (strpbrk(s, accept));
+	int j;
+
+	while (*s != '\0') /*Declaring WHILE*/
+	{
+		j = 0;
+		while (accept[j] != '\0')  /*Evaluating *accept*/
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+
+			j++; /*add j+1*/
+		}
+
+		s++; /*add s+1*/
+	}
+	return (0);
+
 }
