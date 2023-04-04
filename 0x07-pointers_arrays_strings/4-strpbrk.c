101db7dc860bf3a222aab912c6d3a12b4edc2637
Mucyo <arnaudmucyo3@gmail.com>
2023-04-03 17:44:48 -0700

    meme

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,6 +5,7 @@
  *@accept: The string to be searched
  *Return: A pointer to the byte in @s or NULL if the byter is not there
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	return (strpbrk(s, accept));
