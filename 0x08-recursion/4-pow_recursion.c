9af04bf5f54ffdef94ae6b126d9991ae39647975
Mucyo <arnaudmucyo3@gmail.com>
2023-04-05 15:06:22 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,56 @@
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 /**
- *_pow_recursion - A function to do pow
- *@x: number
- *@y: number
+ *sqrt_rec_odd - helper method for odd numbers
+ *@s : odd number
+ *@n: number
+Return: number;
+*/
+int sqrt_help(int s, int n)
+{
+	if (s == (pow(n,2))
+	{
+		return (n);
+	}
+	if (n < 0)
+		reutn (-1);
+	return (sqrt_help(s, n-2));
+}
+
+/**
+ *_sqrt_recursion - function to do recutsive sqrt
+ *@n: number
  *Return: number
  */
 
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	int x;
+	if (n >= 0 && n <= 1 )
+        {
+                return (n);
+        }
+	if (n < 0)
 		return (-1);
-	else if (!y)
+	
+	if (n % 2)		
+		return (sqrt_helper(n, (n - 2)));
+	if (!(n % 2))
 	{
-		return (1);
+		if ((n / x) == x)
+		{
+			return x;
+		}
+		if (n == 2 || (n / 2) ==2)
+		{
+			return (2);
+		}
+		if( x > 1)
+		{
+			return (x * 2);
+		}
+		x = _sqrt_recursion(n/2);
+		return (-1);
 	}
-	return (x * _pow_recursion(x, --y));
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ *factorial - A function to do factorial
+ *@n: number
+ *Return: number
+ */
+
+int factorial(int n)
+
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
