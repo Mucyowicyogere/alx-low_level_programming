744b279bd50b1171e590da8095bd22b445730537
Mucyo <arnaudmucyo3@gmail.com>
2023-04-05 14:22:57 -0700

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ *_pow_recursion - A function to do pow
+ *@x: number
+ *@y: number
+ *Return: number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (!y)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, --y));
+}
