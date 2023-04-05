1a44d6e21a348ac8ad16764090edaf9faba2bc21
Mucyo <arnaudmucyo3@gmail.com>
2023-04-05 15:32:42 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,13 +11,16 @@ int is_prime_number( int n)
 {
 	if (n == 0 || n == 1)
 		return (0);
-	for (int i = 2; i <= (n / 2); ++i) {
+	int i;
+	while (i <= (n / 2)) 
+	{
 
 		if (n % i == 0) 
 		{
 			return 0;
 			break;
 		}
+		i++;
 	}
 	return(1);
 }
