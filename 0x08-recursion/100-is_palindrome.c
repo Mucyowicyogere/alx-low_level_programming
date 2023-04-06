805591c4e981b80009c3cfd7c69c4e1bdbec9c67
Mucyo <arnaudmucyo3@gmail.com>
2023-04-05 17:30:24 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * tick - string counter.
+ * @s: string
+ *
+ * Return: string length.
+ */
+int tick(char *s)
+{
+	if (!*s)
+		return (0);
+	else if (*s)
+		return (tick((s + 1)) + 1);
+}
+/**
+ * track_pali - method to assist is_palindrom
+ * @s: string
+ * @front: front index.
+ * @back: back index.
+ *
+ * Return: 1 or  0.
+ */
+int track_pali(char *s, int fore, int back)
+{
+	if (s[fore] != s[back])
+		return (0);
+	if ((s[fore] == s[back] && fore == back) || (s[fore] == s[back] && fore > back))
+		return (1);
+	else
+		return (track_pali(s, fore + 1, back - 1));
+}
+/**
+ * is_palindrome -  palindrome checking
+ * @s: string
+ *
+ * Return: 1 if palindrome or 0
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	if (*s)
+	{
+		i = tick(s);
+        	return (track_pali(s, 0, i - 1));
+	}
+	if (!(*s))
+		return (1);
+
+}
+
