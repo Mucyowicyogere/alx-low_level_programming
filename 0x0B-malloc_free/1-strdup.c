de7495fc8fbe775312fff297b541a33d41cb74b2
Mucyo <arnaudmucyo3@gmail.com>
2023-04-11 14:02:52 -0700

    create array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*_strdup - to mem allocation
+*@str: string
+*Return: string
+**/
+char *_strdup(char *str)
+{
+	char *ar;
+	int i, len;
+	if (str == NULL)
+		return (NULL);
+	len = strlen(str) + 1;
+	ar = malloc(len * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ar[i] = str[i];
+		i++;
+	}
+//	ar[i] = '\0';
+	return (ar);
+}
