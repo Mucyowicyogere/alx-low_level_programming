49a6a390b2e7a13a8e262541277f621f4fdec37f
Mucyo <arnaudmucyo3@gmail.com>
2023-04-11 14:06:24 -0700

    create array

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	len = strlen(str) + 1;
-	ar = malloc(len * sizeof(char));
+	ar =(char*)malloc(len * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
 	i = 0;
