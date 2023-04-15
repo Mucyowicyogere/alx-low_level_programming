ba5ec6cdae154bdcf0ac16c3d0f407fde759c76f
Mucyo <arnaudmucyo3@gmail.com>
2023-04-15 09:49:44 -0700

    print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*main - to print its name
+*@argc: args number
+*@argv: array
+*Return: 1
+**/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (1);
+}
