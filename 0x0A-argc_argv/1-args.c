0a5237bcba7a4ee897f4cfa927337fb0519da204
Mucyo <arnaudmucyo3@gmail.com>
2023-04-15 10:16:26 -0700

    print arg nbr

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
 *@argc: args number
 *@argv: array
 *Return: 0
-**/
-int main(int argc, char* argv[])
+*ttribute*/
+int main(int argc, char* argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc-1);
 	return (0);
