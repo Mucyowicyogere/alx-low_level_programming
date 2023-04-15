0c7a918b22ad9b9389f8cd284f9b3a6accade61e
Mucyo <arnaudmucyo3@gmail.com>
2023-04-15 13:31:01 -0700

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,10 +15,10 @@ int main(int argc, char **argv )
 	if (argc < 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
-	a =atoi(argv[1]);
+	a = atoi(argv[1]);
 	b = atoi(argv[2]);
-	printf("%d\n", (a*b));
+	printf("%d\n", (a * b));
 	return (0);
 }
