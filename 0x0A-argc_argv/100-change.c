f023a102123081cc822a04daeaeeae16ad4f69b5
Mucyo <arnaudmucyo3@gmail.com>
2023-04-15 15:01:54 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int coins, ar;
 
 	coins = 0;
-	ar =0;
+	ar = 0;
 	
 	if (argc != 2)
 	{
@@ -25,31 +25,31 @@ int main(int argc, char *argv[])
 		printf("%d\n", 0);
 	while(ar > 0)
 	{
-		if (ar >25)
+		if (ar >= 25)
 		{
-			coins += 1;
-			ar -= 25;
+			coins = coins + 1;
+			ar = ar - 25;
 			continue;
 		}
-		if (ar > 10)
+		if (ar >= 10)
 		{
 			ar -= 10;
 			coins += 1;
 			continue;
 		}
-		if (ar > 5)
+		if (ar >= 5)
                 {
                         ar -= 5;
                         coins += 1;
                         continue;
                 }
-		if (ar > 2)
+		if (ar >= 2)
                 {
                         ar -= 2;
                         coins += 1;
                         continue;
                 }
-		if (ar > 1)
+		if (ar >= 1)
                 {
                         ar -= 1;
                         coins += 1;
