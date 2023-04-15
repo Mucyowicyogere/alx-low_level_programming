2b8090b4097c1dfcb49682f86bd9ca88e7413cd2
Mucyo <arnaudmucyo3@gmail.com>
2023-04-15 12:46:00 -0700

    print

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <ctype.h>
 /**
 *main - to add 2 numbers
 *@argc: args number
@@ -8,17 +9,25 @@
 *Return: 0
 **/
 
-int main(int argc, char **argv )
+int main(int argc, char **argv)
 {
 	int a, b;
 
-	if (argc < 3)
+	if(argc < 2)
+	{
+		printf("%d\n", 0);
+		return(1);
+	}
+
+	a = atoi(argv[1]);
+        b = atoi(argv[2]);
+	
+	if((!isdigit(a)) || (!isdigit(b)))
 	{
 		printf("Error\n");
 		return(1);
 	}
-	a =atoi(argv[1]);
-	b = atoi(argv[2]);
+	
 	printf("%d\n", (a+b));
 	return(0);
 }
