0b50ab852cc8ba259e6819a9f34ecfbe61386e84
Mucyo <arnaudmucyo3@gmail.com>
2023-04-19 14:37:05 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+* int_index - to find and return index
+* @array: array
+* @size: size
+* @cmp: function
+* Return: integer
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	for(i = 0; i < size; i++)
+	{
+		if(cmp(array[i]) > 0)
+			return i;
+	}
+}
