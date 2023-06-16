f07329fd32ad631ca05c231f744377c0a884502f
mucyowicyogere <arnaudmucyo3@gmail.com>
2023-06-15 17:17:27 -0700

    malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*malloc_checked - to allocate memory
+*@b: byte size
+*Return: void
+**/
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	if (p != NULL)
+		return p;
+	return NULL;
+}
